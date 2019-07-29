1a34406003c829764a268d9da27cf2c58f7c67b0
Marf Foster <marielleefoster@gmail.com>
2019-07-29 16:38:29 -0500

    two fer exercise

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,2 +1,6 @@
-def two_fer(name):
-    pass
+def two_fer(name=None):
+    if name in ["Alice", "Bob"]:
+        return ("One for %s, one for me." % name)
+    if name == None:
+        return "One for you, one for me."
+
