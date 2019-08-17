a055632c9331ebf9ab092ad7fc9b8a6f5b81d1aa
Marf Foster <marielleefoster@gmail.com>
2019-08-16 23:24:17 -0500

    almost working solution

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,30 @@
 pub fn verse(n: i32) -> String {
-    unimplemented!("emit verse {}", n)
+    let mut ret = "".to_string();
+    if n == 0 {
+        ret = format!("No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.\n");
+    } else if n == 1 {
+        ret = format!("1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.\n");
+    } else if n == 2 {
+        ret = format!("{} bottles of beer on the wall, {} bottles of beer.
+Take one down and pass it around, {} bottle of beer on the wall.\n", n, n, n-1);
+    } else if n > 1 {
+        ret = format!("{} bottles of beer on the wall, {} bottles of beer.
+Take one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1);
+    }
+    ret
 }
 
 pub fn sing(start: i32, end: i32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    let mut ret = "".to_string();
+    if start == end {
+        ret = verse(start)
+    } else {
+        for n in end..start+1 {
+            ret = format!("{}{}\n", verse(n), ret);
+            // ret.push(verse(n));
+        }
+    }
+    ret
 }
