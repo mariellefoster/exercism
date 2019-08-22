e9aac9a92b133ecee1cb9deef1f351f56ae344f4
Marf Foster <marielleefoster@gmail.com>
2019-08-21 20:51:22 -0500

    beer song almost all tests pass

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -2,7 +2,7 @@ pub fn verse(n: i32) -> String {
     let mut ret = "".to_string();
     if n == 0 {
         ret = format!("No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.\n");
+Go to the store and buy some more, 99 bottles of beer on the wall.");
     } else if n == 1 {
         ret = format!("1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.\n");
@@ -21,9 +21,9 @@ pub fn sing(start: i32, end: i32) -> String {
     if start == end {
         ret = verse(start)
     } else {
-        for n in end..start+1 {
-            ret = format!("{}{}\n", verse(n), ret);
-            // ret.push(verse(n));
+        ret = verse(end);
+        for n in end+1..start+1 {
+            ret = format!("{}\n{}", verse(n), ret);
         }
     }
     ret
