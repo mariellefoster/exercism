e80a9f87fbaa6c335e7ede4ae5a8a3a85dd63741
Marf Foster <marielleefoster@gmail.com>
2019-08-28 17:08:19 -0500

    more progress on sum of multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,5 +1,18 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    while factor < limit {
-        let is_present = factors.iter().any(|c| c.position == cell.position);
+    let mut t = factors[0];
+    let final_factors = [u32];
+    for f in factors {
+        // push all multiples
+        while t < limit {
+            let is_present = factors.iter().any(|t| t == &x);
+            if !is_present {
+                final_factors.push(t);
+            }
+            t = t + f;
+        }
     }
+    // while factor < limit {
+
+    //     let is_present = factors.iter().any(|c| c.position == cell.position);
+    // }
 }
