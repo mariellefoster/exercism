ae5e3f814d2e90024194a4e3f7a5b309013c6f37
Marf Foster <marielleefoster@gmail.com>
2019-08-28 16:47:52 -0500

    difference of squares

---
+++ rust/difference-of-squares/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn square_of_sum(n: u32) -> u32 {
+    let sum = n*(n+1) / 2;
+    u32::pow(sum, 2)
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    // n^3/3 + n^2/2 + n/6
+    (n*(n+1)*(2*n+1))/6
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}
