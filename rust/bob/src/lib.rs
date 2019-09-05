d5d5fdb7d51d3ecaf32e7f9ba89046560e93c9ed
Marf Foster <marielleefoster@gmail.com>
2019-09-04 22:03:44 -0500

    bob/src/lib.rs notes

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,20 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let question = boolean function
+
+    let yelling = boolean function
+
+    // if question "Sure."
+
+    // if yelling in caps "Whoa, chill out!"
+
+
+    // if yelling question "Calm down, I know what I'm doing!"
+
+
+    // if nothing "Fine. Be that way!"
+
+
+    // all else "Whatever."
+
+    // use pattern matching?
 }
