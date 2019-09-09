15730e88d4f31911121d998d5265e6e89ea2fadf
Marf Foster <marielleefoster@gmail.com>
2019-09-09 18:51:59 -0500

    bob improvement

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,7 +1,34 @@
 pub fn reply(message: &str) -> &str {
-    let question = boolean function
+    let len = message.len();
+    let question = if let "?" = &message[len-1..] {
+        true
+    } else {
+        false
+    };
 
-    let yelling = boolean function
+    let mut yelling = true;
+    for c in message.chars(){
+        if c.is_lowercase() {
+            yelling = false;
+        }
+    };
+
+    if yelling {
+        if question {
+            return "Calm down, I know what I\'m doing!"
+        }
+        return "Whoa, chill out!"
+    } else if question {
+        return "Sure."
+    } else if message == "" {
+        return "Fine. Be that way!"
+    }
+    return "Whatever."
+    
+
+    // | message | message[message.len()-1] == "?";
+
+    // let yelling = boolean function
 
     // if question "Sure."
 
