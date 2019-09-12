16760ef8e013a9c2ac72da78d037a42e7f4e099d
Marf Foster <marielleefoster@gmail.com>
2019-09-12 17:44:51 -0500

    take three

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,20 +1,3 @@
-use std::vec::Vec;
-
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut vec = vec![];
-
-    for f in factors {
-        let mut m = f;
-        while m < &limit {
-            vec.push(m);
-            let mut m = (m + f);
-        }
-    }
-    vec.sort_unstable();
-    vec.dedup();
-    vec.into_iter().sum()
-    // while factor < limit {
-
-    //     let is_present = factors.iter().any(|c| c.position == cell.position);
-    // }
+    (1..limit).filter(|n| factors.iter().any(|f| f > &0 && n%f == 0)).sum()
 }
