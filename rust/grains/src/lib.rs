ea88889cb2b20f9cb717314b13d35d4dbc412def
Marf Foster <marielleefoster@gmail.com>
2019-09-16 20:25:37 -0500

    solution that works

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,12 @@
 pub fn square(s: u32) -> u64 {
-    2u64.pow(s-1)
+    if s > 0 && s < 65{
+        2u64.pow(s-1)
+    } else {
+        panic!("Square must be between 1 and 64");
+        0
+    }
 }
 
 pub fn total() -> u64 {
-    square(65) - 1
+    18_446_744_073_709_551_615
 }
