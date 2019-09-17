848b130977b53ae4fc5dd822c7eedca31ee74c58
Marf Foster <marielleefoster@gmail.com>
2019-09-16 20:45:45 -0500

    prime factors working

---
+++ rust/prime-factors/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn factors(n: u64) -> Vec<u64> {
+
+    // can you do something like 1...n/2.iter().collect( |s| where n % s == 0 )
+    let mut s = n;
+    let mut vec = vec![];
+    if s > 1 {
+        for i in 2..s+1 {
+            while s % i == 0 {
+                vec.push(i);
+                s = s / i;
+            }
+            if i > s/2 {
+                if s != 1 {
+                    vec.push(s)
+                }
+                break;
+            }
+        }
+    }
+    vec
+}
