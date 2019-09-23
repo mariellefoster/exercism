545f5313e27637f68a890184dc12dbe8f5fb87a4
Marf Foster <marielleefoster@gmail.com>
2019-09-23 15:45:26 -0500

    basic test passes, still have failures

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let digits: Vec<_> = num.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
+    let mut n = 1;
+    let mut sum = 0;
+    for digit in digits {
+        sum += digit.pow(n);
+        n += 1;
+    }
+    if sum == num {
+        true
+    }
+    else {
+        false
+    }
 }
