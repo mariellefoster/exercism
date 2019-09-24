76f8a8c814ab84879fafd96ef3761d3c8b802ead
Marf Foster <marielleefoster@gmail.com>
2019-09-24 09:59:09 -0500

    it works, now with matching

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,15 +1,12 @@
 pub fn is_armstrong_number(num: u32) -> bool {
     let digits: Vec<_> = num.to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
-    let mut n = 1;
+    let n = digits.len() as u32;
     let mut sum = 0;
     for digit in digits {
         sum += digit.pow(n);
-        n += 1;
     }
-    if sum == num {
-        true
-    }
-    else {
-        false
+    match sum {
+        sum if sum == num => true,
+        _ => false,
     }
 }
