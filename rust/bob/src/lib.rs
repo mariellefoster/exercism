733c921e21f0bbe52a536e272a3ca20df4ebb72f
Marf Foster <marielleefoster@gmail.com>
2019-09-25 12:24:34 -0500

    tests passing bob

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,27 +1,33 @@
 pub fn reply(message: &str) -> &str {
+    let message = message.trim_end();
     let len = message.len();
+    if len == 0 {
+        return "Fine. Be that way!"
+    }
     let question = if let "?" = &message[len-1..] {
         true
     } else {
         false
     };
 
-    let mut yelling = true;
+    let mut lowercase = false;
+    let mut uppercase = false;
     for c in message.chars(){
         if c.is_lowercase() {
-            yelling = false;
+            lowercase = true;
+        }
+        if c.is_uppercase() {
+            uppercase = true;
         }
     };
 
-    if yelling {
+    if !lowercase && uppercase {
         if question {
             return "Calm down, I know what I'm doing!"
         }
         return "Whoa, chill out!"
     } else if question {
         return "Sure."
-    } else if message == "" {
-        return "Fine. Be that way!"
     }
     return "Whatever."
 
