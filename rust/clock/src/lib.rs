b1e342967ba00ffc5e00c30ef5c8166eda426225
Marf Foster <marielleefoster@gmail.com>
2019-10-11 10:27:14 -0500

    29 tests passing

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -7,7 +7,7 @@ pub struct Clock {
 
 impl fmt::Debug for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:0>2}:{:0>2}", self.hr, self.min)
+        write!(f, "{:02}:{:02}", self.hr, self.min)
     }
 }
 
@@ -17,30 +17,26 @@ impl PartialEq for Clock {
     }
 }
 
-// impl fmt::Display for Clock {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "Clock {{ Hours: {}, Minutes: {} }}", self.hr, self.min)
-//     }
-// }
-
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}:{}", self.hr, self.min)
+        write!(f, "{:0>2}:{:0>2}", self.hr, self.min)
     }
 }
 
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let new_hours = hours.abs() % 24;
-        let new_mins = minutes.abs() % 60;
+        let new_hours = ((hours % 24) + 24) % 24;
+        let new_mins = ((minutes % 60) + 60) % 60;
         let new_clock = Clock { hr : new_hours, min : new_mins };
         new_clock
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let new_min = (minutes + self.min).abs() % 60;
-        Clock::new(self.hr, new_min)
+        let add_hours = minutes / 60;
+        let new_min = ((((minutes % 60) + self.min) % 60) + 60) % 60;
+        let hours = ((add_hours + self.hr % 24) + 24) % 24;
+        Clock::new(hours, new_min)
     }
 
 }
