3ebbdea5de062fad068c7f7f2f67f9068d9c3192
Marf Foster <marielleefoster@gmail.com>
2019-10-20 13:46:12 -0500

    tests all passed

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -16,12 +16,13 @@ impl fmt::Display for Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let new_clock = Clock { min: (hours * 60 + minutes).rem_euclid(24 * 60) };
+        let new_mins = (((hours * 60 + minutes) % 1440) + 1440) % 1440;
+        let new_clock = Clock { min: new_mins };
         new_clock
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let new_min = (self.min + minutes).rem_euclid(24 * 60);
+        let new_min = (((self.min + minutes) % 1440) + 1440) % 1440;
         Clock::new(0, new_min)
     }
 
