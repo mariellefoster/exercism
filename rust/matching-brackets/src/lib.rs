82909d2f7decb1d24ee0d82854a4d7fec6a2ce0e
Marf Foster <marielleefoster@gmail.com>
2019-10-28 19:33:11 -0500

    all tests compile and pass

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -2,29 +2,30 @@ pub fn brackets_are_balanced(string: &str) -> bool {
     let mut characters = Vec::new();
 
     for character in string.chars() {
+        println!("{:?}", characters);
         match character {
             '{' => characters.push(character), //add to list
             '[' => characters.push(character), //add to list
             '(' => characters.push(character), //add to list
-            _ => continue,
+            _ => (),
         }
 
         if character == '}' {
             let popped_char = characters.pop();
             if popped_char != Some('{') {
-                break;
+                characters.push(character);
             }
         } //pop from list
         else if character == ']' {
             let popped_char = characters.pop();
             if popped_char != Some('[') {
-                break;
+                characters.push(character);
             }
         }
         else if character == ')' {
             let popped_char = characters.pop();
             if popped_char != Some('(') {
-                break;
+                characters.push(character);
             }
         }
     }
