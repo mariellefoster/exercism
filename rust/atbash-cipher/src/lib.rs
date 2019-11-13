cbad80560a2c349567af102cfb928419ad451a68
Marf Foster <marielleefoster@gmail.com>
2019-11-13 07:39:18 -0600

    all tests passing

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,42 +1,43 @@
+/// "Helper" with the Atbash cipher.
+pub fn helper(c : i16) -> char {
+    match c {
+        97 ... 123 => ((c - 122).abs() as u8 + 97) as char,
+        65 ... 91  => ((c - 90).abs() as u8 + 97) as char,
+        _ => (c as u8) as char,
+    }
+}
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    // read in string as characters
-    // create new string to return
-    let mut result = String::from("");;
-    // convert to lowercase
-    // strip whitespace and punctuation
-    let clean_plain: String = plain.split_whitespace().collect();
-    for character in clean_plain.chars() {
-        if !character.is_ascii() || character.is_ascii_punctuation() {
+    let mut result = String::from("");
+    let mut f = 0;
+    for character in plain.chars() {
+        if !character.is_ascii() 
+                || character.is_ascii_punctuation()
+                || character.is_ascii_whitespace() {
             continue;
         }
         let c = character as i16;
-        let d = match c {
-            97 ... 123 => ((c - 122).abs() as u8 + 97) as char,
-            65 ... 91  => ((c - 90).abs() as u8 + 97) as char,
-            _ => continue,
-        };
-        println!("{:?}", d);
+        let d = helper(c);
+        f += 1;
         result.push(d);
+        if f % 5 == 0 {
+            result.push(' ');
+        }
     }
-    result
-
-    // loop through characters, converting to ascii
-    // if it's a number, just keep the number
-
-    // convert ascii char to its opposite. 97-122 is the
-    // current range for lower case letters
-    // so minus 97, add -25, take abs value, use that
-
-    // add 97, convert to ascii and then character
-    // add to string
-    // create chunker function which inserts spaces every five
-    // chars
-    // unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    result.trim_end().to_string()
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    unimplemented!("Decoding of {:?} in Atbash cipher.", cipher);
+    let mut result = String::from("");
+    for character in cipher.chars() {
+        if character.is_ascii_whitespace() {
+            continue;
+        }
+        let c = character as i16;
+        let d = helper(c);
+        result.push(d);
+    }
+    result
 }
