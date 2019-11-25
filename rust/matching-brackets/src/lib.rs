46dca91c6c9b4d8f449bd3f1e1fd29b555df7fd5
Marf Foster <marielleefoster@gmail.com>
2019-11-25 17:23:17 -0600

    shortening matching brackets to be much more elegant

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -2,40 +2,14 @@ pub fn brackets_are_balanced(string: &str) -> bool {
     let mut characters = Vec::new();
 
     for character in string.chars() {
-        println!("{:?}", characters);
         match character {
-            '{' => characters.push(character), //add to list
-            '[' => characters.push(character), //add to list
-            '(' => characters.push(character), //add to list
+            '{' | '[' | '(' => characters.push(character),
+            '}' => if characters.pop() != Some('{') { return false },
+            ']' => if characters.pop() != Some('[') { return false },
+            ')' => if characters.pop() != Some('(') { return false },
             _ => (),
         }
-
-        if !is_back_bracket(character) {
-            continue;
-        } else {
-            let popped_char = characters.pop();
-            match (popped_char, character) {
-                (Some('{'), '}') => (),
-                (Some('['), ']') => (),
-                (Some('('), ')') => (),
-                (_, _) => characters.push(character)
-            };
-        }
     }
 
-    if characters.len() != 0 {
-        false
-    } else {
-        true
-    }
-}
-
-pub fn is_back_bracket(b: char) -> bool {
-    // turn this into "member of list?"
-    match b {
-        '}' => true,
-        ')' => true,
-        ']' => true,
-        _ => false
-    }
+    characters.is_empty()
 }
