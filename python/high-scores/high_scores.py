a197ca3d3731ebd3c2615bd6719ae205af7d1291
Marf Foster <marielleefoster@gmail.com>
2020-02-02 22:45:46 +0100

    almost done

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,13 @@
+def latest(scores):
+    return scores[len(scores)-1]
+
+
+def personal_best(scores):
+    return max(scores)
+
+
+def personal_top_three(scores):
+    if len(scores) > 3:
+        return sorted(scores)[:-3]
+    else:
+        return sorted(scores)[::-1]
