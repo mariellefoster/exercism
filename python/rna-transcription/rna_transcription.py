0d253d488001349424bb6ff763876432969300c0
Marf Foster <marielleefoster@gmail.com>
2020-02-07 15:52:57 +0100

    rna solution

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,3 +1,6 @@
 def to_rna(dna_strand):
-    pass
-    # use an iterator?
+    rna_converter = {"G": "C", "C": "G", "T": "A", "A": "U"}
+    try:
+        return "".join([rna_converter[d] for d in dna_strand])
+    except Exception as e:
+        raise ValueError("Invalid input")
