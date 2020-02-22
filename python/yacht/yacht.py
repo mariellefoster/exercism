2a824a5aa2f0ca9012099d30d4cd95a01e9de1b6
Marf <marielleefoster@gmail.com>
2020-02-22 18:42:16 +0100

    almost all tests passing

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -14,18 +14,18 @@ You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
 
 # Score categories.
 # Change the values as you see fit.
-YACHT = None
+YACHT = 'y'
 ONES = 1
 TWOS = 2
 THREES = 3
 FOURS = 4
 FIVES = 5
 SIXES = 6
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
+FULL_HOUSE = 'fh'
+FOUR_OF_A_KIND = 'f'
 LITTLE_STRAIGHT = "ls"
 BIG_STRAIGHT = "bs"
-CHOICE = None
+CHOICE = 'ch'
 
 
 def score(dice, category):
@@ -40,6 +40,22 @@ def score(dice, category):
     elif category in [LITTLE_STRAIGHT, BIG_STRAIGHT]:
         dice = sorted(set(dice))
         if len(dice) == 5:
-            return 30
+            if LITTLE_STRAIGHT and dice[0] != 1 or \
+                BIG_STRAIGHT and dice[4] != 6:
+                return 30
+    elif category == CHOICE:
+        return sum(dice)
+    elif category == FULL_HOUSE:
+        if dice[0] == dice[2] and dice[3] == dice[4] or \
+            dice[0] == dice[1] and dice[2] == dice[4]:
+            if dice[0] != dice[4]:
+                return sum(dice)
+    elif category == YACHT:
+        if dice[0] == dice[4]:
+            return 50
+    elif category == FOUR_OF_A_KIND:
+        if dice[0] == dice[3] or dice[1] == dice[4]:
+            return 4*dice[1]
+
 
     return score
