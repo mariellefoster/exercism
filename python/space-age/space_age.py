2c1edd82656ebc1ec19e7bfa688f2c9a16ca5eca
Marf Foster <marielleefoster@gmail.com>
2020-03-16 16:57:45 +0100

    space age beginning

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,6 +1,29 @@
 class SpaceAge:
     def __init__(self, seconds):
+        # - Earth: orbital period 365.25 Earth days, or 31557600 seconds
+        self.seconds
+        self.earth = 31557600
+        self.mercury = earth*0.2408467
+        self.venus
+
+    def on_earth(self):
+        return self.seconds / self.earth
+
+    def on_mercury(self):
+        return self.seconds / (self.earth * 0.2408467)
+
+    def on_venus(self):
+        return self.seconds / (self.earth * 0.61519726)
+
+    def on_mars(self):
         pass
+       
+       # - Venus: orbital period 0.61519726 Earth years
+       # - Mars: orbital period 1.8808158 Earth years
+       # - Jupiter: orbital period 11.862615 Earth years
+       # - Saturn: orbital period 29.447498 Earth years
+       # - Uranus: orbital period 84.016846 Earth years
+       # - Neptune: orbital period 164.79132 Earth years
 
 
 # load all planets
