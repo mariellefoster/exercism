10998b6d07b6d5fdf13698852df4e52aa74f877d
Marf Foster <marielleefoster@gmail.com>
2020-03-22 17:33:26 +0100

    space age

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,25 +1,33 @@
 class SpaceAge:
     def __init__(self, seconds):
         # - Earth: orbital period 365.25 Earth days, or 31557600 seconds
-        self.seconds
+        self.seconds = seconds
         self.earth = 31557600
-        self.mercury = earth*0.2408467
-        self.venus
 
     def on_earth(self):
-        return self.seconds / self.earth
+        return round(self.seconds / self.earth, 2)
 
     def on_mercury(self):
-        return self.seconds / (self.earth * 0.2408467)
+        return round(self.seconds / (self.earth * 0.2408467), 2)
 
     def on_venus(self):
-        return self.seconds / (self.earth * 0.61519726)
+        return round(self.seconds / (self.earth * 0.61519726), 2)
 
     def on_mars(self):
-        pass
-       
-       # - Venus: orbital period 0.61519726 Earth years
-       # - Mars: orbital period 1.8808158 Earth years
+        return round(self.seconds / (self.earth * 1.8808158), 2)
+
+    def on_jupiter(self):
+        return round(self.seconds / (self.earth * 11.86261), 2)
+
+    def on_saturn(self):
+        return round(self.seconds / (self.earth * 29.447498), 2)
+
+    def on_uranus(self):
+        return round(self.seconds / (self.earth * 84.016846), 2)
+
+    def on_neptune(self):
+        return round(self.seconds / (self.earth * 164.79132 ), 2)
+
        # - Jupiter: orbital period 11.862615 Earth years
        # - Saturn: orbital period 29.447498 Earth years
        # - Uranus: orbital period 84.016846 Earth years
