06363ac702fa1079abc8ca6e98ae03cf49fe4377
Marf Foster <marielleefoster@gmail.com>
2020-04-03 23:16:08 +0200

    now optimized for storage

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,14 +1,13 @@
 class Matrix:
     def __init__(self, matrix_string):
         self.rows = []
-        self.columns = []
         self.read_in_matrix(matrix_string)
 
     def row(self, index):
         return self.rows[index-1]
 
     def column(self, index):
-        return self.columns[index-1]
+        return [row[index-1] for row in self.rows]
 
     def read_in_matrix(self, matrix_string):
         rows = matrix_string.split('\n')
@@ -19,4 +18,3 @@ class Matrix:
             for i in range(len(row_strs)):
                 num = int(row_strs[i])
                 self.rows[row_i].append(num)
-                self.columns[i].append(num)
