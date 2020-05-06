6de803deafbb2e690e7c0725c5cbd4069c5f72cb
Marf Foster <marielleefoster@gmail.com>
2020-05-06 22:07:39 +0200

    protein translation beginning

---
+++ python/protein-translation/protein_translation.py
@@ -0,0 +1,28 @@
+# Codon   Protein
+
+# UGU, UGC    Cysteine
+# UGG Tryptophan
+# UAA, UAG, UGA   STOP
+
+Codon = {
+    "AUG" : "Methionine",
+    "UUU" : "Phenylalanine",
+    "UUC" : "Phenylalanine",
+    "UUA" : "Leucine",
+    "UUG" : "Leucine",
+    "UCU" : "Serine",
+    "UCC" : "Serine",
+    "UCA" : "Serine",
+    "UCG" : "Serine",
+    "UAU" : "Tyrosine",
+    "UAC" : "Tyrosine",
+    "UGU" : "Cysteine",
+    "UGC" : "Cysteine",
+    "UGG" : "Tryptophan",
+    "UAA" : "STOP",
+    "UAG" : "STOP",
+    "UGA" : "STOP",
+}
+
+def proteins(strand):
+    []
