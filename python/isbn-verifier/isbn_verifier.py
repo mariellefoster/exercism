3e1248f79c50b9c19356ee396461792bf14530c6
Marf Foster <marielleefoster@gmail.com>
2020-05-12 00:21:02 +0200

    isbn work in progress

---
+++ python/isbn-verifier/isbn_verifier.py
@@ -0,0 +1,7 @@
+def is_valid(isbn):
+    
+    # print(isbn)
+    # print([int(isbn[i]) * (10-i) for i in range(len(str(isbn)))])
+    # print(sum([int(isbn[i]) * (10-i) for i in range(len(str(isbn)))]))
+
+    # return sum([if isbn[i].isnumber() : isbn[i] * (10-i) for i in range(len(str(isbn)))]) % 11 == 0
