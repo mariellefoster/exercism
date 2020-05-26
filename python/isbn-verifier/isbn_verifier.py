bd8ed58d44a7dfc18ab8754034b6b03ac67661f8
Marf Foster <marielleefoster@gmail.com>
2020-05-26 22:13:08 +0200

    retooled, much better

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,11 +1,13 @@
 def is_valid(isbn):
-    isbn = isbn.replace('-', '')
-    if len(isbn) == 10 and (isbn[-1].isdigit() or isbn[-1] == "X") and isbn[:-1].isdigit():
-        isbn_sum = sum([int(isbn[i]) * (10-i) for i in range(0, len(isbn)-1)])
-        if isbn[-1] == "X":
-            isbn_sum += 10
-        else:
-            isbn_sum += int(isbn[-1])
+    isbn = list(isbn.replace('-', ''))
+    if len(isbn) != 10:
+        return False
+
+    if isbn[-1] == "X":
+        isbn[-1] = '10'
+    
+    try:
+        isbn_sum = sum([int(isbn[i]) * (10-i) for i in range(len(isbn))])
         return isbn_sum % 11 == 0
-    else:
+    except ValueError:
         return False
