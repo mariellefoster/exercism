99ad384c2da540559e3e25c13e9ed6e3f81f26ef
Marf Foster <marielleefoster@gmail.com>
2020-06-04 00:03:53 +0200

    sum_of_squares

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,10 @@
-def square_of_sum(number):
-    return (number * (number + 1)/2)**2
+def square_of_sum(n):
+    return (n * (n + 1)/2)**2
 
 
-def sum_of_squares(number):
-    
+def sum_of_squares(n):
+    return (n*(n + 1)*(2*n + 1))/6
 
 
-def difference_of_squares(number):
+def difference_of_squares(n):
     pass
