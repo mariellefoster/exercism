7d02e387e8e6cd1f1d8c7179a45520b75691869e
Marf Foster <marielleefoster@gmail.com>
2020-10-05 20:10:22 +0200

    bob working

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,19 @@
+QUESTION = "Sure."
+YELL = "Whoa, chill out!"
+YELL_QUESTION = "Calm down, I know what I'm doing!"
+SILENCE = "Fine. Be that way!"
+ALL_ELSE = "Whatever."
+
+
 def response(hey_bob):
-    pass
+    hey_bob = hey_bob.strip()
+    if hey_bob == "":
+        return SILENCE
+    end_token = hey_bob[-1]
+    if hey_bob.isupper():
+        if end_token == "?":
+            return YELL_QUESTION
+        return YELL
+    if end_token == "?":
+        return QUESTION
+    return ALL_ELSE
