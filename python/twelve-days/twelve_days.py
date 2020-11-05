ab6428673a7ba8266542422a9c88f76665f5a962
Marf Foster <marielleefoster@gmail.com>
2020-11-05 12:55:59 +0100

    small update

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,14 +1,14 @@
-GIFTS = ['twelve Drummers Drumming,',
-         'eleven Pipers Piping,',
-         'ten Lords-a-Leaping,',
-         'nine Ladies Dancing,',
-         'eight Maids-a-Milking,',
-         'seven Swans-a-Swimming,',
-         'six Geese-a-Laying,',
-         'five Gold Rings,',
-         'four Calling Birds,',
-         'three French Hens,',
-         'two Turtle Doves,',
+GIFTS = ['twelve Drummers Drumming',
+         'eleven Pipers Piping',
+         'ten Lords-a-Leaping',
+         'nine Ladies Dancing',
+         'eight Maids-a-Milking',
+         'seven Swans-a-Swimming',
+         'six Geese-a-Laying',
+         'five Gold Rings',
+         'four Calling Birds',
+         'three French Hens',
+         'two Turtle Doves',
          'a Partridge in a Pear Tree.']
 
 DAYS = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth",
@@ -21,6 +21,6 @@ def recite(start_verse, end_verse):
         gift_verse = (f"On the {DAYS[day-1]} day of Christmas my true love gave to me: ")
         if(day > 1):
             gift_list[-1] = "and " + gift_list[-1]
-        gift_verse += " ".join(gift_list)
+        gift_verse += ", ".join(gift_list)
         final_list.append(gift_verse)
     return final_list
