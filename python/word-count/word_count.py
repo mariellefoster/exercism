49863426f99679a3f0d72967b83896ac5557b1b1
Marf Foster <marielleefoster@gmail.com>
2020-11-25 22:36:43 +0100

    all but one pass

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,14 +1,16 @@
 import re
 
 def count_words(sentence):
-    sentence = re.sub(r"[^\w\d'\s]+", '', sentence)
+    sentence = re.sub(r"[^\w\d'\_\s]+", ' ', sentence)
     sentence = sentence.lower()
     word_list = sentence.split()
     word_dict = {}
     for w in word_list:
-        w
+        while w.endswith("'") and w.startswith("'"):
+            w = w[1:-1]
         if w in word_dict:
             word_dict[w] += 1
         else:
             word_dict[w] = 1
+    print(word_dict)
     return word_dict
