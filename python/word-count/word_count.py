2d7fa8439db6baf11a5b5a41a77058defafc3e7f
Marf Foster <marielleefoster@gmail.com>
2020-11-25 22:31:19 +0100

    11/16 pass

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,8 +1,12 @@
+import re
+
 def count_words(sentence):
-    low_sen = sentence.lower()
-    word_list = low_sen.split()
+    sentence = re.sub(r"[^\w\d'\s]+", '', sentence)
+    sentence = sentence.lower()
+    word_list = sentence.split()
     word_dict = {}
     for w in word_list:
+        w
         if w in word_dict:
             word_dict[w] += 1
         else:
