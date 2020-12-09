b26a1185217d063efb73f82be74211a598c188bd
Marf Foster <marielleefoster@gmail.com>
2020-12-09 23:11:51 +0100

    finished word count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,16 +1,11 @@
 import re
 
 def count_words(sentence):
-    sentence = re.sub(r"[^\w\d'\_\s]+", ' ', sentence)
-    sentence = sentence.lower()
-    word_list = sentence.split()
+    sentence = re.sub(r"[^A-Za-z0-9\d'\s]+", ' ', sentence)
+    word_list = sentence.lower().split()
     word_dict = {}
     for w in word_list:
         while w.endswith("'") and w.startswith("'"):
             w = w[1:-1]
-        if w in word_dict:
-            word_dict[w] += 1
-        else:
-            word_dict[w] = 1
-    print(word_dict)
+        word_dict[w] = word_dict.get(w, 0) + 1
     return word_dict
