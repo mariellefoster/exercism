3741cdc42ea3c08571dbdb8ad14609c7d522d953
Marf <marielleefoster@gmail.com>
2021-01-03 22:13:10 +0100

    notes

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -11,5 +11,7 @@ bool is_armstrong_number(int candidate)
     {
         return true;
     }
+    // Need something to measure the digits, and also go through each digit
+    // maybe something recursive that does mod by 10?
     return false;
 }
