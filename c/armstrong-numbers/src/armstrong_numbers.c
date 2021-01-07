e45a6ae7e9646b48c29a5cb7f3b061a09a496277
Marf <marielleefoster@gmail.com>
2021-01-07 23:58:30 +0100

    all tests passing

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,28 +1,17 @@
 #include "armstrong_numbers.h"
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 bool is_armstrong_number(int candidate)
 {
     int digits = floor (log10 (abs (candidate))) + 1;
     int comparison = 0;
     int cand_copy = candidate;
-    while(cand_copy > 1){
-        int curr_dig = candidate % 10;
+    while(cand_copy > 0){
+        int curr_dig = cand_copy % 10;
         comparison += pow(curr_dig, digits);
         cand_copy /= 10;
     }
     return comparison == candidate;
-
-    if ( candidate == 0 )
-    {
-        return true;
-    }
-    if ( candidate >=0 && candidate <=9 )
-    {
-        return true;
-    }
-    // Need something to measure the digits, and also go through each digit
-    // maybe something recursive that does mod by 10?
-    return false;
 }
