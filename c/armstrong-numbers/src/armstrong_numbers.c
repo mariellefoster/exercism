fe3979a8b0a8accf5f7800dcbd3629813b77d930
Marf <marielleefoster@gmail.com>
2021-01-07 23:54:28 +0100

    6 out of 9 passing

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,8 +1,19 @@
 #include "armstrong_numbers.h"
-
+#include <math.h>
+#include <stdlib.h>
 
 bool is_armstrong_number(int candidate)
 {
+    int digits = floor (log10 (abs (candidate))) + 1;
+    int comparison = 0;
+    int cand_copy = candidate;
+    while(cand_copy > 1){
+        int curr_dig = candidate % 10;
+        comparison += pow(curr_dig, digits);
+        cand_copy /= 10;
+    }
+    return comparison == candidate;
+
     if ( candidate == 0 )
     {
         return true;
